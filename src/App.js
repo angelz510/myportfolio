c50f1fd8c0b972e42d1fd2d53aededbc429448dc
Angel R <angelzrod4@gmail.com>
2020-11-16 11:43:18 -0800

    added title and typewriter effect. pretty cool

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 //components
 import MyNavbar from "./components/navbar/navbar";
 import MyCarousel from './components/carousel/carousel';
+import TitleMsg from './components/title-msg/title-msg';
 import './App.css';
 
 const App = () => {
   return (<div>
     <MyNavbar />
     <MyCarousel />
-    Angel's New Attempt</div>);
+    <TitleMsg />
+    </div>);
 };
 
 export default App;
