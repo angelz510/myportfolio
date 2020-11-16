4fff6495a1e082bae4787db8ccbbbaa3af36d7c6
Angel R <angelzrod4@gmail.com>
2020-11-16 10:56:04 -0800

    added carousel on home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //components
 import MyNavbar from "./components/navbar/navbar";
+import MyCarousel from './components/carousel/carousel';
 import './App.css';
 
 const App = () => {
   return (<div>
     <MyNavbar />
+    <MyCarousel />
     Angel's New Attempt</div>);
 };
 
---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './carousel.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+import bart from '../../assets/carousel/bart.jpg';
+import oakport from '../../assets/carousel/oakport.jpg';
+
+const MyCarousel = () => {
+  return (
+    <div id="home">
+      <Carousel controls={false} indicators interval={2500} pauseOnHover={false}>
+        <Carousel.Item>
+          <img className="d-block w-100 custome img" src={oakport} alt="First slide"/>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={bart} alt="Second slide"/>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  )
+}
+
+export default MyCarousel;
