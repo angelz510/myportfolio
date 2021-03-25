44090f5bde0e2bf15ec32accf03d6fb030321eb5
Angel R <angelzrod4@gmail.com>
2021-03-25 11:22:33 -0700

    added img-fluid to photo

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -20,7 +20,7 @@ const About = () => {
             {/* Profile Photo */}
             <Col xs={12} md={6}>
               <Row className='justify-content-center mb-2 mr-2'>
-                <Image src={Profile} alt="profile" fluid/>
+                <Image src={Profile} alt="profile" className="img-fluid"/>
               </Row>
             </Col>
 
